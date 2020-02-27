7c574210f37abaf983af322b009f2550143f36e4
Pargev <aghabekyan.pargev@gmail.com>
2020-02-27 18:10:19 +0400

    added seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use App\Traits\GenerateRandomString;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'surname' => "Super",
+            'email' => 'admin' . '@gmail.com',
+            'password' => bcrypt('admin'),
+            'password_show' => 'admin',
+            'sync' => 1,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
