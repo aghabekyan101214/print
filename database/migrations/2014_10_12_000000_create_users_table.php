5cb9c9105a425fe713d0e7ee8f72d2e825db6494
unknown <aghabekyan.pargev@gmail.com>
2020-03-25 17:51:32 +0400

    added apis

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name', 191);
             $table->string('email', 191)->unique();
-            $table->string('password');
+            $table->string('password', 191);
             $table->rememberToken();
             $table->timestamps();
         });
